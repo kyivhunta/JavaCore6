a30bba4f6ceb05dc308a8d5e789f08051a4f6ed6
serejen'ka shevchenko <sport1k89@gmail.com>
2017-01-29 19:42:13 +0200

    HomeWork011

---
+++ src/main/java/com/serega/practice/module01/task011/FirstClass.java
@@ -0,0 +1,8 @@
+package com.serega.practice.module01.task011;
+
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на Java");
+    }
+}
