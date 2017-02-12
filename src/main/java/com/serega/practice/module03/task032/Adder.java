21c8b7f5ed10745a77201100c69e01ffeffbf7b8
serejen'ka shevchenko <sport1k89@gmail.com>
2017-02-12 14:02:46 +0200

    FixedHomeWork032

--- src/main/java/com/serega/practice/module03/task032/Adder.java
+++ src/main/java/com/serega/practice/module03/task032/Adder.java
@@ -3,18 +3,14 @@ package com.serega.practice.module03.task032;
 
 public class Adder extends Arithmetic {
 
-    boolean check(Integer a, Integer b) {
-        if (a >= b) return true;
-        else return false;
+    public boolean check(Integer a, Integer b) {
+        return a >= b;
     }
-
 
     public static void main(String[] args) {
 
         Adder obj = new Adder();
         System.out.println(obj.add(5, 5) + "\n" + obj.check(5, 6));
 
-
     }
-
 }
--- src/main/java/com/serega/practice/module03/task032/Arithmetic.java
+++ src/main/java/com/serega/practice/module03/task032/Arithmetic.java
@@ -3,8 +3,7 @@ package com.serega.practice.module03.task032;
 public class Arithmetic {
 
 
-    int add(Integer a, Integer b) {
-        int c = a + b;
-        return c;
+    public int add(Integer a, Integer b) {
+        return a+b;
     }
 }
